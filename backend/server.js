49d755aebc7e58e5dd660b3bce0c00b63e583161
Keerti Saini <sainikeerti0202@gmail.com>
2026-09-18 09:37:04 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:5173",
-            "https://youtube-watch-party-n8fwe7lr9-cartoon-projects.vercel.app"
+            "https://youtube-watch-party-5bm3.vercel.app/"
         ],
         methods: ["GET", "POST"]
     }
